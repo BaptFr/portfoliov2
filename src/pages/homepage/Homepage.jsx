bc58fb9c9eeb78cdca09131aa24bbd72cbdf6ed7
BaptFr <baptistesalazar@gmail.com>
2025-01-13 14:33:09 +0100

    -landingPage --Slills&formationSections ---struc&content&resp *style

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -11,7 +11,7 @@ function Homepage () {
             <section className='section d-flex flex-row justify-content-sa align-items-center wrap'>
                 <article className='article border-style'>
                     <h1> Bienvenue sur mon portfolio ! </h1>
-                    <p>Après une ancienne vie dans le commerce, j'ai effectué une reconversion
+                    <p>Après une ancienne vie dans le commerce, j&apos; ai effectué une reconversion
                     professionnelle pour me plonger dans le développement web. <br/>
                     Explorez ici une sélection de projets qui témoignent de mon parcours,
                     de ma formation et de mes compétences.
@@ -38,21 +38,73 @@ function Homepage () {
             {/* SOFTSKILLS section*/}
             <section className='section'>
             <h2>SOFT SKILLS</h2>
-            <div className={`${styles.softSkillsContainer} p-30`}>
-                    <ul className='d-flex flex-row justify-content-sa wrap'>
-                        <li>Espirt d'équipe</li>
-                        <li>Autonomie</li>
-                        <li>Adaptabilité</li>
-                        <li>Curiosité</li>
-                        <li>Persévérance</li>
+            <div className={`${styles.softSkillsContainer} p-30 `}>
+                    <ul className='d-flex flex-row justify-content-sa wrap gap-10'>
+                        <li>ESPRIT D&apos; EQUIPE</li>
+                        <li>AUTONOMIE</li>
+                        <li>ADAPTABILITE</li>
+                        <li>CURIOSITE</li>
+                        <li>PERSEVERANCE</li>
                     </ul>
                 </div>
             </section>
 
 
 
-            <section className='section'>
-                <h2>FORMATION</h2>
+            <h2 id={styles.formationTitle}>FORMATION</h2>
+            <section className={styles.formationSection}>
+                <div className={styles.timeline}>
+                    <div className={styles.timelineEmptyRight}>
+                        <p>Décembre 2023</p>
+                    </div>
+                    <div className={styles.timelineMiddle}>
+                        <div className={styles.timelineCircle}></div>
+                    </div>
+                    <div className={`${styles.timelineComponent} ${styles.timelineContent} ${styles.timelineContentL}`}>
+                        <h4>Meta Front-End Developer- Professional Certificate</h4>
+                        <p>Meta 9 courses program</p>
+                    </div>
+                    <div className={`${styles.timelineComponent} ${styles.timelineContent} ${styles.timelineContentR}`}>
+                            <h4>Certification Professionnelle de Développeur web</h4>
+                            <p>Open Classrooms</p>
+                    </div>
+                    <div className={styles.timelineMiddle}>
+                        <div className={styles.timelineCircle}></div>
+                    </div>
+                    <div className={styles.timelineEmpty}>
+                    <p>Juillet 2023</p>
+                    </div>
+                    <div className={styles.timelineEmptyRight}>
+                    <p>2015</p>
+                    </div>
+                    <div className={styles.timelineMiddle}>
+                        <div className={styles.timelineCircle}></div>
+                    </div>
+                    <div className={`${styles.timelineComponent} ${styles.timelineContent} ${styles.timelineContentL}}`}>
+                        <h4>Titre Professionnnel de Manager < br/>  d&apos; Univers Marchand</h4>
+                        <p>RNCP - Niveau 5 (BAC +2)</p>
+                    </div>
+                    <div className={`${styles.timelineComponent} ${styles.timelineContent} ${styles.timelineContentR}`}>
+                            <h4>Ecole Nationale des Sous-Officers d&apos; Active</h4>
+                            <p>Armée de Terre</p>
+                    </div>
+                    <div className={styles.timelineMiddle}>
+                        <div className={styles.timelineCircle}></div>
+                    </div>
+                    <div className={styles.timelineEmpty}>
+                    <p>2009</p>
+                    </div>
+                    <div className={styles.timelineEmptyRight}>
+                    <p>2007</p>
+                    </div>
+                    <div className={styles.timelineMiddle}>
+                        <div className={styles.timelineCircle}></div>
+                    </div>
+                    <div className={`${styles.timelineComponent} ${styles.timelineContent} ${styles.timelineContentL}`}>
+                        <h4>Baccalauréat </h4>
+                        <p>Série Scientifique</p>
+                    </div>
+                </div>
             </section>
         </div>
     )
