856c55fe68cb82df494560fc8c40443c38aaf779
BaptFr <baptistesalazar@gmail.com>
2025-01-16 22:09:02 +0100

    -ontactPage --cotactForm ---EmailJS OK work on return message and style

--- src/pages/contactpage/ContactPage.jsx
+++ src/pages/contactpage/ContactPage.jsx
@@ -1,22 +1,23 @@
 import styles from './ContactPage.module.scss';
-
+import ContactImage from '../../assets/images/contact-image.webp';
+import ContactForm from '../../components/ContactForm.jsx';
 function ContactPage () {
 
     return (
-        <div>
-            <section className='section d-flex flex-row justify-content-sa align-items-center wrap'>
-            <article className='article border-style'>
-                <h1> TEST t </h1>
-                <p>Intro contact
-                </p>
-            </article>
-            <img  alt='contact-illustration' className='pageImage' />
+        <div className ='d-flex flex-column justify-content-sb p-20'>
+              <h1> ME CONTACTER </h1>
+            <img src={ContactImage} alt='contact-illustration' className='pageImage' />
+            <section className={`${styles.contactContainer} b2 section d-flex flex-row justify-content-sb align-items-center wrap`}>
+                <article className='article border-style'>
+                    <p>Vous souhaitez échanger à propos d'un projet ou une autre demande ? < br/>
+                        N'hésitez surtout pas à me contacter soit par mes réseaux, soit directement en remplissant
+                        le formulaire et je vous répondrai dans les plus brefs délais !
+                    </p>
+                </article>
+            <div>
+                <ContactForm />
+            </div>
             </section>
-
-        <section className='section d-flex flex-row all-center'>
-             LOGOS 
-            Formulaire
-        </section>
         </div>
     )
 };
