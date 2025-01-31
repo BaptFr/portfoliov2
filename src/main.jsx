a807773f4cdd90d01ef35ba9d6d0eed7275addf0
BaptFr <baptistesalazar@gmail.com>
2025-01-31 21:28:48 +0100

    modifs build

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,6 @@ import './assets/styles/index.scss';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router.jsx';
 
-
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={ router } >
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    outDir: 'dist', // Assurez-vous que Vite place les fichiers de build dans un dossier "build"
+  }
 })
