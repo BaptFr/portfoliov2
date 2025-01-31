0f37ef5505c3170002dd1c21f0d0529c40c03d64
BaptFr <baptistesalazar@gmail.com>
2025-01-31 21:44:06 +0100

    modifs for deplo

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Assurez-vous que Vite place les fichiers de build dans un dossier "build"
-  }
+    outDir: 'dist',
+  },
 })
