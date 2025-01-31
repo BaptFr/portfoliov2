2afd8e1c3694e38f4d092631f908fcbc9ab41c93
BaptFr <baptistesalazar@gmail.com>
2025-01-31 21:56:58 +0100

    modifs deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,14 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // Si tu veux charger des variables globales ou des mixins, tu peux ajouter un "prependData"
+        additionalData: `@import "./src/assets/styles/variables.scss";`
+      }
+    }
+  }
   build: {
     outDir: 'dist',
   },
