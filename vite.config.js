2c2fdaef23949af769c484db9d75077ddffbb751
BaptFr <baptistesalazar@gmail.com>
2025-02-01 14:01:17 +0100

    dpl gh

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-   base: '/portfoliov2/'
+  build: {
+    outDir: 'dist'
+  },
+  base: '/portfoliov2/'
 })
