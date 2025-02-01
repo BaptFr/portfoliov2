146e89baf0a78cb32a404c08117ab7768e9c2a21
BaptFr <baptistesalazar@gmail.com>
2025-02-01 12:57:39 +0100

    deply changes

--- src/router.jsx
+++ src/router.jsx
@@ -1,11 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 
-const LandingPage =  import('./pages/landingpage/Landingpage.jsx'));
-const PresentationPage = import('./pages/presentationpage/Presentationpage'));
-const ProjectsPage=  import('./pages/projectspage/Projectspage'));
-const ContactPage =  import('./pages/contactpage/ContactPage'));
-const ErrorPage =  import('./pages/errorpage/Errorpage'));
+const LandingPage =  import('./pages/landingpage/Landingpage.jsx');
+const PresentationPage = import('./pages/presentationpage/Presentationpage');
+const ProjectsPage=  import('./pages/projectspage/Projectspage');
+const ContactPage =  import('./pages/contactpage/ContactPage');
+const ErrorPage =  import('./pages/errorpage/Errorpage');
 
 
 export const router = createBrowserRouter([
